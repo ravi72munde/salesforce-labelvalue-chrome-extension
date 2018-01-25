bb2769b553fba9ee7637e61c44d8037715bd4961
Ravi Munde <ravi72munde@gmail.com>
2018-01-25 18:38:20 -0500

    Code cleanup

--- js/background.js
+++ js/background.js
@@ -1,18 +1,9 @@
 
 chrome.browserAction.setPopup({popup:"popup.html"});
 chrome.runtime.onInstalled.addListener(function(info) {
-    // if(info.details == "update" || info.details == "install") {
-        // chrome.browserAction.setBadgeText({text:"1"});
-    // }
 })
 
 
 chrome.browserAction.onClicked.addListener(function() {
     chrome.browserAction.setPopup({popup:"popup.html"});
 });
-/*chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-		var response = request.payload;
-		sendResponse({'value':response});
-  });
-*/
